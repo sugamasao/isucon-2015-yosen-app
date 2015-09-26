ced1e21dc328c7218ce6245d871d308ae78ee301
Yuichiro Saito <saito@heartbeats.jp>
2015-09-26 16:55:35 +0900

    20件までに絞る

--- ruby/app.rb
+++ ruby/app.rb
@@ -204,7 +204,7 @@ SQL
     settings.logger.info '/: comments_for_me end'
 
     entries_of_friends = []
-    db.query('SELECT * FROM entries ORDER BY created_at DESC LIMIT 1000').each do |entry|
+    db.query('SELECT * FROM entries ORDER BY created_at DESC LIMIT 20').each do |entry|
           next unless is_friend?(entry[:user_id])
           entry[:title] = entry[:body].split(/\n/).first
           entries_of_friends << entry
@@ -213,7 +213,7 @@ SQL
     settings.logger.info '/: entries_of_friends end'
 
     comments_of_friends = []
-    db.query('SELECT * FROM comments ORDER BY created_at DESC LIMIT 1000').each do |comment|
+    db.query('SELECT * FROM comments ORDER BY created_at DESC LIMIT 20').each do |comment|
       next unless is_friend?(comment[:user_id])
       entry = db.xquery('SELECT * FROM entries WHERE id = ?', comment[:entry_id]).first
       entry[:is_private] = (entry[:private] == 1)

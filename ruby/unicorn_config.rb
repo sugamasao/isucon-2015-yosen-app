170671f900221ef71d1a204507c9700e141482ff
sugamasao <sugamasao@gmail.com>
2015-09-26 18:31:04 +0900

    unicorn loggerを停止

--- ruby/unicorn_config.rb
+++ ruby/unicorn_config.rb
@@ -4,5 +4,5 @@ listen 8080
 pid "/home/isucon/webapp/ruby/unicorn.pid"
 listen "/tmp/unicorn.sock"
 
-stderr_path File.expand_path('log/unicorn_stderr.log', __dir__)
-stdout_path File.expand_path('log/unicorn_stdout.log', __dir__)
+#stderr_path File.expand_path('log/unicorn_stderr.log', __dir__)
+#stdout_path File.expand_path('log/unicorn_stdout.log', __dir__)
